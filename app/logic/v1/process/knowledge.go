0beed7360659dd61c17e0581e1fa6b5b0ae7b9e5
boyce <w@ojbk.io>
2025-02-03 23:46:55 +0800

    improving chunk

--- app/logic/v1/process/knowledge.go
+++ app/logic/v1/process/knowledge.go
@@ -264,12 +264,47 @@ func (p *KnowledgeProcess) processEmbedding(req *EmbeddingRequest) {
 	sw := mark.NewSensitiveWork()
 	// content := sw.Do(req.data.Summary)
 
+	var chunksData []types.KnowledgeChunk
+
 	ctx, cancel := context.WithTimeout(req.ctx, time.Minute*5)
 	defer cancel()
-	chunksData, err := p.core.Store().KnowledgeChunkStore().List(ctx, req.data.SpaceID, req.data.ID)
-	if err != nil {
-		slog.Error("Failed to list knowledge chuns", append(logAttrs, slog.String("error", err.Error()))...)
-		return
+
+	if req.data.Kind == types.KNOWLEDGE_KIND_CHUNK {
+		// type KnowledgeChunk struct {
+		// 	ID             string `json:"id" db:"id"`                           // 主键，字符串类型
+		// 	KnowledgeID    string `json:"knowledge_id" db:"knowledge_id"`       // 知识点ID
+		// 	SpaceID        string `json:"space_id" db:"space_id"`               // 空间ID
+		// 	UserID         string `json:"user_id" db:"user_id"`                 // 用户ID
+		// 	Chunk          string `json:"chunk" db:"chunk"`                     // 知识片段
+		// 	OriginalLength int    `json:"original_length" db:"original_length"` // 原文长度
+		// 	UpdatedAt      int64  `json:"updated_at" db:"updated_at"`           // 更新时间
+		// 	CreatedAt      int64  `json:"created_at" db:"created_at"`           // 创建时间
+		// }
+
+		markdownContent := string(req.data.Content)
+		if req.data.ContentType == types.KNOWLEDGE_CONTENT_TYPE_BLOCKS {
+			markdownContent, err = utils.ConvertEditorJSBlocksToMarkdown(json.RawMessage(req.data.Content))
+			if err != nil {
+				slog.Error("Failed to convert editor blocks to markdown", append(logAttrs, slog.String("error", err.Error()))...)
+				return
+			}
+		}
+		chunksData = append(chunksData, types.KnowledgeChunk{
+			ID:             req.data.ID,
+			KnowledgeID:    req.data.ID,
+			SpaceID:        req.data.SpaceID,
+			UserID:         req.data.UserID,
+			Chunk:          markdownContent,
+			OriginalLength: len([]rune(markdownContent)),
+			UpdatedAt:      time.Now().Unix(),
+			CreatedAt:      time.Now().Unix(),
+		})
+	} else {
+		chunksData, err = p.core.Store().KnowledgeChunkStore().List(ctx, req.data.SpaceID, req.data.ID)
+		if err != nil {
+			slog.Error("Failed to list knowledge chuns", append(logAttrs, slog.String("error", err.Error()))...)
+			return
+		}
 	}
 
 	var (
